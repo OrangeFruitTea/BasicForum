f77dd7e104a5284aa64aa8cb50b5a6fccc20bef2
OrangeFruitTea <118669479+OrangeFruitTea@users.noreply.github.com>
2023-11-23 17:25:08 +0800

    修改一部分测试内容，尝试使用ToolBar添加头部导航栏未果。

--- app/src/main/java/com/example/basicfourm/ui/NewPostActivity.java
+++ app/src/main/java/com/example/basicfourm/ui/NewPostActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
