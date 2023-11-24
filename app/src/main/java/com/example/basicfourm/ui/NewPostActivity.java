9976b0344ce2c9847b32ca90e054babd46b2406a
OrangeFruitTea <118669479+OrangeFruitTea@users.noreply.github.com>
2023-11-24 15:25:50 +0800

    重要更新 重写新建帖子界面的顶部导航栏，弃用先前的按钮布局

--- app/src/main/java/com/example/basicfourm/ui/NewPostActivity.java
+++ app/src/main/java/com/example/basicfourm/ui/NewPostActivity.java
@@ -2,15 +2,15 @@ package com.example.basicfourm.ui;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.basicfourm.R;
@@ -23,8 +23,8 @@ import java.util.Date;
 public class NewPostActivity extends AppCompatActivity {
 
     private Toolbar header;
-    private Button btnSendPost;
-    private Button btnReturn;
+//    private Button btnSendPost;
+//    private Button btnReturn;
     private EditText PostInputTitle;
     private EditText PostInputSubTitle;
     private EditText PostInputContent;
@@ -33,55 +33,93 @@ public class NewPostActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newpost);
-        btnReturn = findViewById(R.id.btn_return);
-        btnSendPost= findViewById(R.id.btn_sendPost);
+//        btnReturn = findViewById(R.id.btn_return);
+//        btnSendPost= findViewById(R.id.btn_sendPost);
         PostInputContent = findViewById(R.id.newpost_input_content);
         PostInputTitle = findViewById(R.id.newpost_input_title);
         PostInputSubTitle = findViewById(R.id.newpost_input_subTitle);
+        header = findViewById(R.id.header);
+        setSupportActionBar(header);
 
 
         //设置按钮监听事件
-        btnSendPost.setOnClickListener(new View.OnClickListener() {
+        //对导航栏的返回按钮点击事件进行监听
+        header.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //Toast.makeText(NewPostActivity.this, "1", Toast.LENGTH_SHORT).show();
-                if(isPostValid())
-                {
-                    String title = PostInputTitle.getText().toString().trim();
-                    String content = PostInputContent.getText().toString().trim();
-                    String subtitle = PostInputSubTitle.getText().toString().trim();
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
-                    Date date = new Date(System.currentTimeMillis());
-
-                    PostList thePost = new PostList(title,subtitle,content,LoginActivity.LoginUser,simpleDateFormat.format(date).toString(),ObjectId);
-                    boolean flag = thePost.save();
-                    if(!flag)
-                    {
-                        Toast.makeText(NewPostActivity.this, "发帖失败", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-
-                    Toast.makeText(NewPostActivity.this,"发帖成功", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        btnReturn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 PostList thePost =LitePal.where("objectID=?",String.valueOf(1)).find(PostList.class).get(1);
                 Toast.makeText(NewPostActivity.this,thePost.getTitle(), Toast.LENGTH_SHORT).show();
-                //Toast.makeText(NewPostActivity.this, "2", Toast.LENGTH_SHORT).show();
             }
         });
+//        btnSendPost.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //Toast.makeText(NewPostActivity.this, "1", Toast.LENGTH_SHORT).show();
+//                if(isPostValid())
+//                {
+//                    String title = PostInputTitle.getText().toString().trim();
+//                    String content = PostInputContent.getText().toString().trim();
+//                    String subtitle = PostInputSubTitle.getText().toString().trim();
+//                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+//                    Date date = new Date(System.currentTimeMillis());
+//
+//                    PostList thePost = new PostList(title,subtitle,content,LoginActivity.LoginUser,simpleDateFormat.format(date).toString(),ObjectId);
+//                    boolean flag = thePost.save();
+//                    if(!flag)
+//                    {
+//                        Toast.makeText(NewPostActivity.this, "发帖失败", Toast.LENGTH_SHORT).show();
+//                        return;
+//                    }
+//
+//                    Toast.makeText(NewPostActivity.this,"发帖成功", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//        btnReturn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                PostList thePost =LitePal.where("objectID=?",String.valueOf(1)).find(PostList.class).get(1);
+//                Toast.makeText(NewPostActivity.this,thePost.getTitle(), Toast.LENGTH_SHORT).show();
+//                //Toast.makeText(NewPostActivity.this, "2", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if (item.getItemId() == R.id.btn_send_post) {
-//
-//        }
-//    }
+    //将顶部导航栏右侧的菜单加载至activity内部
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_newpost, menu);
+        return true;
+    }
+
+    //对导航栏的发送帖子按钮点击事件进行重写
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.btn_send_post) {
+            if(isPostValid())
+            {
+                String title = PostInputTitle.getText().toString().trim();
+                String content = PostInputContent.getText().toString().trim();
+                String subtitle = PostInputSubTitle.getText().toString().trim();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+                Date date = new Date(System.currentTimeMillis());
+
+                PostList thePost = new PostList(title,subtitle,content,LoginActivity.LoginUser,simpleDateFormat.format(date).toString(),ObjectId);
+                boolean flag = thePost.save();
+                if(!flag)
+                {
+                    Toast.makeText(NewPostActivity.this, "发帖失败", Toast.LENGTH_SHORT).show();
+                }
+
+                Toast.makeText(NewPostActivity.this,"发帖成功", Toast.LENGTH_SHORT).show();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     public boolean isPostValid() {
         String title = PostInputTitle.getText().toString().trim();
