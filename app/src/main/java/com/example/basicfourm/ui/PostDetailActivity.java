8fc67085a2f70b5568206deaad3337ca5998d238
OrangeFruitTea <118669479+OrangeFruitTea@users.noreply.github.com>
2023-12-11 10:50:28 +0800

    更新帖子详情界面显示的布局

--- app/src/main/java/com/example/basicfourm/ui/PostDetailActivity.java
+++ app/src/main/java/com/example/basicfourm/ui/PostDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.basicfourm.ui;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,17 +16,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.basicfourm.R;
 import com.google.android.material.navigation.NavigationView;
 
 import org.litepal.LitePal;
 
+import java.util.List;
+
 public class PostDetailActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "PostDetailActivity";
     private Toolbar toolbar;
     private int postId;
+    private ReplyAdapter adapter;
+    //获得的本帖子界面的数据信息selfInfo
     private PostList selfInfo;
+    //该帖子下的所有评论
+    private List<ReplyList> allReply;
+
+    //布局控件相关
+    private SwipeRefreshLayout refreshLayout;
+    private RecyclerView layoutAllComment;
 
 
 
@@ -38,11 +52,43 @@ public class PostDetailActivity extends AppCompatActivity implements NavigationV
         toolbar = (Toolbar) findViewById(R.id.header_post_detail);
         setSupportActionBar(toolbar);
 
+        //点击跳转时将对应帖子信息赋值到此布局
         Intent intent = getIntent();
         postId = intent.getIntExtra("postId", -1);
-
         selfInfo = LitePal.find(PostList.class, postId);
+
+        //设置刷新操作和回调函数
+        refreshLayout = findViewById(R.id.layout_comment_refresh);
+        //设置下拉刷新图标颜色
+        refreshLayout.setColorSchemeColors(Color.parseColor("#2E9DFD"));
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshData();
+            }
+        });
+
+        layoutAllComment = findViewById(R.id.view_all_comment);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        allReply = LoginActivity.thePostManager.getAllCommentForCertainPostWithObject(selfInfo);
+        adapter = new ReplyAdapter(allReply);
+        if (layoutAllComment != null) {
+            layoutAllComment.setAdapter(adapter);
+            layoutAllComment.setLayoutManager(layoutManager);
+        }
     }
+
+    //帖子内的数据更新
+    private void refreshData() {
+        if (adapter != null) {
+            //获取对应所有评论的数据源
+            allReply = LoginActivity.thePostManager.getAllCommentForCertainPostWithObject(selfInfo);
+            adapter.setData(allReply);
+        }
+        adapter.notifyDataSetChanged();
+        refreshLayout.setRefreshing(false);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_post_info, menu);
---
+++ app/src/main/java/com/example/basicfourm/ui/ReplyAdapter.java
@@ -0,0 +1,71 @@
+package com.example.basicfourm.ui;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.basicfourm.R;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class ReplyAdapter extends RecyclerView.Adapter {
+    private List<ReplyList> replyArray;
+    public ReplyAdapter(List<ReplyList> array) {
+        setData(array);
+    }
+
+    public void setData(List<ReplyList> array) {
+        replyArray = array;
+    }
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_comment, parent, false);
+        return new ReplyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ReplyList reply = replyArray.get(position);
+        if (holder instanceof ReplyViewHolder) {
+            ReplyViewHolder replyViewHolder = (ReplyAdapter.ReplyViewHolder) holder;
+            replyViewHolder.CommentTitle.setText(reply.getTitle());
+            replyViewHolder.CommentSubTitle.setText(reply.getTitle());
+            replyViewHolder.CommentTime.setText(reply.getTimeInReply());
+            replyViewHolder.CommentAuthor.setText(reply.getReplyer().getUserName());
+            replyViewHolder.CommentInfo.setText(reply.getReply());
+            replyViewHolder.CommentFloor.setText(String.valueOf(reply.getFloor()));
+
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return replyArray.size();
+    }
+
+    class ReplyViewHolder extends RecyclerView.ViewHolder {
+        TextView CommentAuthor;
+        TextView CommentTime;
+        TextView CommentTitle;
+        TextView CommentSubTitle;
+        TextView CommentInfo;
+        TextView CommentFloor;
+        public ReplyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            CommentAuthor = itemView.findViewById(R.id.textView_comment_username);
+            CommentTime = itemView.findViewById(R.id.textView_comment_time);
+            CommentTitle= itemView.findViewById(R.id.textView_comment_title);
+            CommentSubTitle = itemView.findViewById(R.id.textView_comment_sub_title);
+            CommentInfo= itemView.findViewById(R.id.TextView_comment_main_text);
+            CommentFloor = itemView.findViewById(R.id.textView_comment_floor);
+        }
+    }
+}
